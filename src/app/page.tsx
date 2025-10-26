bda453ea6ba298ddfa90285e7694581ffb09c45b
Vansh Kharidia <vanshkharidia7@gmail.com>
2025-10-27 03:50:12 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button';
 
 const Page = () => {
-  return (
+return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <Button>
         Click Me
